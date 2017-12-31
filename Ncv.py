533e36a15c020c9b5384cb2bb4bc40d940c40ee3
Pantzan <panagiotist@mail.com>
2017-12-31 01:44:15 +0000

    add source code

---
+++ Ncv.py
@@ -0,0 +1,90 @@
+from Knn import Knn
+
+def NestedCrossVal(X,y,foldK,nns,dists,mySeed):
+    np.random.seed(mySeed)
+    accuracy_fold=[]   
+    indices = np.random.permutation(X.shape[0])
+    bins=np.array_split(indices, foldK)
+    sum_matrix = []
+    assert(foldK==len(bins))
+    for i in range(0,foldK):
+        foldTrain=[] 
+        foldTest=[]
+        foldVal=[]
+
+        # split the X to train, test and validation folds
+        foldTest.extend(bins[i])
+        if(i != foldK-1):
+            foldVal.extend(bins[i+1])
+        else:
+            foldVal.extend(bins[0])  
+        
+        # don't add dublicates
+        for j in range(0,len(bins)):
+            if(i != j and i+1 != j):
+                if(i == foldK-1 and j == 0):                   
+                    continue
+                foldTrain.extend(bins[j])
+
+        assert not np.intersect1d(foldTest,foldVal)
+        assert not np.intersect1d(foldTrain,foldTest)
+        assert not np.intersect1d(foldTrain,foldVal)
+     
+        bestDistance=''
+        bestNN=-1 
+        bestAccuracy=-10
+        
+        #loop to all available parametres and find the best to train the model
+        for d in range(0, len(dists)):           
+            for k in nns:
+                knn = MyKnn(k, dists[d])
+                knn.fit(X[foldTrain], y[foldTrain])              
+                
+                y_pred = knn.predict(X[foldVal])
+                acc = knn.accuracy(y[foldVal], y_pred)
+                #it prints all folds results. 
+#                 print('-- validation test, N', k, 'Dist', dists[d],'accuracy ',acc )
+                if acc > bestAccuracy:
+                    bestAccuracy = acc
+                    bestDistance = dists[d]
+                    bestNN = k
+                    
+        print('** val for this fold, best NN', bestNN, 'best Dist', bestDistance)
+        
+        newTrain = foldTrain 
+        newTrain.extend(foldVal)       
+        
+        # call the Knn and train with the best found parametres
+        knn = MyKnn(bestNN, metric=bestDistance)
+        knn.fit(X[newTrain], y[newTrain])                
+        y_pred = knn.predict(X[foldTest])
+        acc = knn.accuracy(y[foldTest], y_pred)
+        accuracy_fold.append(acc)      
+        print('** End of test for this fold (',i+1,'), accuracy', acc)
+        cm = knn.confMat(y[foldVal], y_pred, len(np.unique(y)))
+        
+        # append matrices to list for later use
+        sum_matrix.append(cm)
+        print(cm)
+        
+          # The sklearn KNN classifier for comparison purposes
+#         from sklearn.neighbors import KNeighborsClassifier
+#         knn2 = KNeighborsClassifier(n_neighbors=80, metric='euclidean')
+#         knn2.fit(X[foldTrain], y[foldTrain])
+#         y_pred2 = knn2.predict(X[foldTest])
+#         acc2 = knn.accuracy(y[foldTest], y_pred)
+#         print("2222", knn.accuracy(y[foldTest], y_pred2))
+#         print(knn.confMat(y[foldVal], y_pred, len(np.unique(y))))
+
+    print('==== Final Cross-val on test on this fold with NN', bestNN, 'dist', bestDistance, ' accuracy ',knn.accuracy(y[foldTest],y_pred))
+    print('==== Final Confusion Matrix')
+    
+    #print the total confusion matrix
+    print(sum(sum_matrix))
+    return accuracy_fold
+    
+
+# print the results
+accuracy_fold=NestedCrossVal(X,y,5,list(range(1,11)),['euclidean','absolute'],mySeed)
+# print(np.mean(accuracy_fold))
+print('==== Standard Deviation:', np.std(accuracy_fold))
---
+++ TestKnn.py
@@ -0,0 +1,49 @@
+from Knn import Knn
+# Load the IRIS dataset, as in the labs
+# %matplotlib inline
+
+
+from sklearn import datasets
+import numpy as np
+import matplotlib.pyplot as plt
+
+#import k-nn classifier
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score
+import operator
+
+
+iris = datasets.load_iris()
+
+#view a description of the dataset (uncomment next line to do so)
+#print(iris.DESCR)
+
+#Set X equal to features, Y equal to the targets
+
+X=iris.data 
+y=iris.target 
+
+mySeed=1234567
+
+#initialize random seed generator 
+np.random.seed(mySeed)
+
+#we add some random noise to our data to make the task more challenging
+X=X+np.random.normal(0,0.5,X.shape)
+
+np.random.seed(mySeed)
+indices= np.random.permutation(X.shape[0]) 
+bins=np.array_split(indices,2) # we  just need a training and testing set here
+foldTrain=bins[0]
+foldTest=bins[1]
+knn=Knn(10,'euclidean')
+knn.fit(X[foldTrain],y[foldTrain])
+y_pred=knn.predict(X[foldTest])
+a = np.where(y_pred != y[foldTest])
+print(knn.accuracy(y[foldTest],y_pred))
+# print(knn.confMat())
+# print(knn.confMat(y[foldTest],y_pred,len(np.unique(y))))
